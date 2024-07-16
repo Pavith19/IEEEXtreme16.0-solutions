99e6684289bb297c2b4c3a90a1a19405502960ef
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-16 18:07:21 +0530

    Update scheduler.py

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -1,1 +1,12 @@
+# To get 100% points, you have to use Pypy3 instead of Python3
+jobs, workers = map(int, input().split())
 
+times = list(map(int, input().split()))
+
+if workers > 1:
+    print(2 ** max(times)%1000000007)
+else:
+    total = 0
+    for i in times:
+        total += 2 ** i
+    print(total%1000000007)
