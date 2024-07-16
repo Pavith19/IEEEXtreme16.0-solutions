04a50cf70ea059076539c3633543f53538db16c7
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-16 18:02:37 +0530

    Update pizza.py

--- Pizza Cutter/pizza.py
+++ Pizza Cutter/pizza.py
@@ -1,1 +1,13 @@
+tests = int(input())
 
+for test in range(tests):
+    angles = list(map(int, input().split()))
+    del angles[0]
+    cuts = set()
+    
+    for i in range(len(angles)):
+        cuts.add(angles[i]%180)
+        
+    if len(cuts) == 0:
+        print(1)
+    else:print(len(cuts)*2)
