4e96ea16bb2a3b26cce5c1eb503bb66541262272
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-16 18:12:17 +0530

    Update schedulerredux.py

--- Scheduler Redux/schedulerredux.py
+++ Scheduler Redux/schedulerredux.py
@@ -1,1 +1,47 @@
+from collections import defaultdict
 
+mod = 1000000007
+
+num_jobs, num_workers = map(int, input().split())
+
+jobs = list(map(int, input().split()))
+
+if num_workers == 1:
+    sum = 0
+    
+    for i in range(num_jobs):
+        sum += pow(2, jobs[i], mod)
+        
+    print(sum % mod)
+    
+else:
+    jobs.sort(reverse = True)
+    
+    number_of_each_job = defaultdict(int)
+    
+    max_time = jobs[0]
+    times = []
+    times.append(max_time)
+    
+    for i in range(num_jobs):
+        number_of_each_job[jobs[i]] += 1
+        
+        x = jobs[i]
+        
+        while number_of_each_job[x] == 2 and x != max_time:
+            number_of_each_job[x] = 0
+            x += 1
+            number_of_each_job[x] += 1
+            
+        if number_of_each_job[max_time] == num_workers:
+            if i < num_jobs - 1:
+                times.append(jobs[i + 1])
+                number_of_each_job[max_time] = 0
+                max_time = jobs[i + 1]
+                
+    sum = 0
+    
+    for time in times:
+        sum += pow(2, time, mod)
+        
+    print(sum % mod)
