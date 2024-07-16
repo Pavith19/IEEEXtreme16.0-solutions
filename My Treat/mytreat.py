fe05d8abc4ed422dcd495aa8af575d8b8255f580
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-16 18:03:33 +0530

    Update mytreat.py

--- My Treat/mytreat.py
+++ My Treat/mytreat.py
@@ -1,1 +1,29 @@
+tests = int(input())
 
+for x in range(tests):
+    meals_given = int(input())
+    
+    names = {}
+    
+    for y in range(meals_given):
+        meal = input().split()
+        
+        by_whom = meal[0]
+        count = int(meal[1])
+        to_whom = meal[2:]
+        
+        if by_whom not in names:
+            names[by_whom] = count
+        else:
+            names[by_whom] += count
+            
+        for i in to_whom:
+            if i not in names:
+                names[i] = -1
+            else:
+                names[i] -= 1
+        
+    meals_to_buy = sum([x for x in names.values() if x > 0])
+    days = max(names.values())
+    
+    print(meals_to_buy, days)       
